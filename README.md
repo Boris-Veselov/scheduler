85d422eb62c3ec848ef8e697c9c32c6f0177cfb0
Boris Veselov <borisveselov@yahoo.com>
2022-03-07 20:40:54 -0500

    readme update and screenshot added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Work Day Scheduler 
 Simple calendar application that allows a user to save events for each hour of the day. This app features dynamically updated HTML and CSS powered by jQuery.
+
+https://veselavalava.github.io/scheduler/
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,6 @@ var currentTime = moment()
 timeNow.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a");
 
 
+var workday = JSON.parse(localStorage.getItem("workday")) 
 
-$(".saveBtn").on("click", function() {
-    var $parent = $(this).closest("[data-hour]");
-
-    var hour = $parent.data("hour");
-    
-    var value = $parent.find("textarea").val().trim();
-
-    schedule[hour] = value;
-
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-});
+localStorage.setItem("workday", JSON.stringify(workday));
